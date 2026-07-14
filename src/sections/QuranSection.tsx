dcba6f0fbfb6d25004bdc777108f4c4c49e097f4
Siful Bahri <sifulbahri.14@gmail.com>
2026-07-14 17:20:27 +0700

    update

--- src/sections/QuranSection.tsx
+++ src/sections/QuranSection.tsx
@@ -46,7 +46,7 @@ export function QuranSection() {
 وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُم مِّنْ أَنفُسِكُمْ أَزْوَاجًا لِّتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُم مَّوَدَّةً وَرَحْمَةً ۚ إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِّقَوْمٍ يَتَفَكَّرُونَ
           </p>
 
-          <Paragraph className="mt-12 text-lg leading-9">
+          <Paragraph className="mt-12 text-base leading-9">
             "Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri,
              agar kamu cenderung dan merasa tenteram kepadanya. Dia menjadikan di antaramu rasa kasih dan sayang. 
              Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir."
